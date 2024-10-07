fef947d7015ac81fb0cd4a22a71c5ff38626df5f
Timothy Lee <timothyl@adobe.com>
2024-10-07 13:45:11 -0700

    Initial commit

---
+++ blocks/hero/hero.js
@@ -0,0 +1,16 @@
+/**
+ * loads and decorates the hero
+ * @param {Element} block The hero block element
+ */
+
+console.log ("Hello Helix 5")
+
+export default async function decorate(block) {
+  // Target the third child div to add button-primary class
+  const div = block.children[2];
+
+  if (div) {
+    const btnContainer = div.querySelector('.button-container');
+    btnContainer.querySelector('a').classList.add('button-primary');
+  }
+}
